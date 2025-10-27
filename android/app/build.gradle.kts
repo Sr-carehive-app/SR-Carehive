c61188385d4d574d112b6b3a16feac3b685bf7cb
Hridesh-Srivastava <hrideshsrivastavaa@outlook.com>
2025-10-28 00:41:27 +0530

    refactor: Update application ID to match Play Store requirements

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.care12"
+        // Updated to unique application ID for Play Store
+        applicationId = "com.serechi.srcarehive"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
